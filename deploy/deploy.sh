a7150f0cfa11881e4c7d59c6db594b8480e01b7f
Linus Bäckman <linus@refragged.com>
2019-09-25 16:34:41 +0200

    Deploy script: fix last ssh step user bug

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@ git config --global push.default matching
 git remote add deploy ssh://qwnu-frontend@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
-ssh apps@$IP -p $PORT <<EOF
+ssh qwnu-frontend@$IP -p $PORT <<EOF
     cd $DEPLOY_DIR
     yarn build
 EOF
