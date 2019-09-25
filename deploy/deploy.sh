cde03255a4b49f2424f9f0713b00365f84cd30c4
Linus Bäckman <linus@refragged.com>
2019-09-25 18:32:16 +0200

    Deploy script changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@ git config --global push.default matching
 git remote add deploy ssh://qwnu-frontend@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
-ssh qwnu-frontend@$IP -p $PORT <<EOF
+ssh -n qwnu-frontend@$IP -p $PORT <<EOF
     cd $DEPLOY_DIR
     yarn build
 EOF
