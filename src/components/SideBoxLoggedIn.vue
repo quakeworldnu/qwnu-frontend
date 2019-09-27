51801cf13f34c83fc3f4b3c412ad2499a51a2501
Linus Bäckman <linus@refragged.com>
2019-09-27 19:28:56 +0200

    Show both create/view links in logged-in-menu

--- src/components/SideBoxLoggedIn.vue
+++ src/components/SideBoxLoggedIn.vue
@@ -8,11 +8,15 @@
             <div class="m-0 box-content">
                 <p>
                     <router-link to="/profile/edit" v-if="$can('edit_user_profile')">Edit profile</router-link><br/>
-                    <router-link to="/admin/users" v-if="$can('list_users')">Admin users</router-link><br/>
-                    <router-link to="/admin/articles" v-if="$can('list_articles')">Admin articles</router-link><br/>
-                    <router-link to="/admin/comments" v-if="$can('list_comments')">Admin comments</router-link><br/>
-                    <router-link to="/admin/forum-categories" v-if="$can('list_forum_categories')">Admin forum categories</router-link><br/>
-                    <router-link to="/admin/forums" v-if="$can('list_forums')">Admin forums</router-link>
+                    <router-link to="/admin/user" v-if="$can('create_user')">Create users</router-link><br/>
+                    <router-link to="/admin/users" v-if="$can('list_users')">View users</router-link><br/>
+                    <router-link to="/admin/article" v-if="$can('create_article')">Create article</router-link><br/>
+                    <router-link to="/admin/articles" v-if="$can('list_articles')">View articles</router-link><br/>
+                    <router-link to="/admin/comments" v-if="$can('list_comments')">View comments</router-link><br/>
+                    <router-link to="/admin/forum-category" v-if="$can('create_forum_category')">Create forum categories</router-link><br/>
+                    <router-link to="/admin/forum-categories" v-if="$can('list_forum_categories')">View forum categories</router-link><br/>
+                    <router-link to="/admin/forum" v-if="$can('create_forum')">Create forum</router-link><br/>
+                    <router-link to="/admin/forums" v-if="$can('list_forums')">View forums</router-link>
                 </p>
                 <button
                     type="submit"
