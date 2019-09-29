bb9a84b9d6306bcb76f3db820f093a105e37740e
Linus Bäckman <linus@refragged.com>
2019-09-29 23:07:26 +0200

    Remove yarn steps from deploy script Moved to git post-receive hook.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,3 @@
 git config --global push.default matching
 git remote add deploy ssh://qwnu-frontend@$IP:$PORT$DEPLOY_DIR
 git push deploy master
-
-ssh -n qwnu-frontend@$IP -p $PORT <<EOF
-    cd $DEPLOY_DIR
-    yarn install --frozen-lockfile
-    yarn build
-EOF
